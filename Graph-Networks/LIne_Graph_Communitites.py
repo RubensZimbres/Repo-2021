4dce59bb1e77ec35a036e8e2a9e0cc8302574ec1
Rubens Zimbres <rubenszmm@gmail.com>
2021-01-23 14:30:31 -0300

    Create LIne_Graph_Communitites.py

---
+++ Graph-Networks/LIne_Graph_Communitites.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 23 12:08:57 2021
+
+@author: theone
+"""
+
+import torch
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+
+import dgl
+from dgl.data import citation_graph as citegrh
+
+data = citegrh.load_cora()
+
+G = dgl.DGLGraph(data.graph)
+
+labels = th.tensor(data.labels)
+
+# find all the nodes labeled with class 0
+label0_nodes = th.nonzero(labels == 0).squeeze()
+# find all the edges pointing to class 0 nodes
+src, _ = G.in_edges(label0_nodes)
+src_labels = labels[src]
+# find all the edges whose both endpoints are in class 0
+intra_src = th.nonzero(src_labels == 0)
+print('Intra-class edges percent: %.4f' % (len(intra_src) / len(src_labels)))
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+train_set = dgl.data.CoraBinary()
+G1, pmpd1, label1 = train_set[1]
+nx_G1 = G1.to_networkx()
+
+def visualize(labels, g):
+    pos = nx.spring_layout(g, seed=1)
+    plt.figure(figsize=(8, 8))
+    plt.axis('off')
+    nx.draw_networkx(g, pos=pos, node_size=50, cmap=plt.get_cmap('coolwarm'),
+                     node_color=labels, edge_color='k',
+                     arrows=False, width=0.5, style='dotted', with_labels=False)
+visualize(label1, nx_G1)
+
+import dgl.function as fn
+def aggregate_radius(radius, g, z):
+    # initializing list to collect message passing result
+    z_list = []
+    g.ndata['z'] = z
+    # pulling message from 1-hop neighbourhood
+    g.update_all(fn.copy_src(src='z', out='m'), fn.sum(msg='m', out='z'))
+    z_list.append(g.ndata['z'])
+    for i in range(radius - 1):
+        for j in range(2 ** i):
+            #pulling message from 2^j neighborhood
+            g.update_all(fn.copy_src(src='z', out='m'), fn.sum(msg='m', out='z'))
+        z_list.append(g.ndata['z'])
+    return z_list
+
+class LGNNCore(nn.Module):
+    def __init__(self, in_feats, out_feats, radius):
+        super(LGNNCore, self).__init__()
+        self.out_feats = out_feats
+        self.radius = radius
+
+        self.linear_prev = nn.Linear(in_feats, out_feats)
+        self.linear_deg = nn.Linear(in_feats, out_feats)
+        self.linear_radius = nn.ModuleList(
+                [nn.Linear(in_feats, out_feats) for i in range(radius)])
+        self.linear_fuse = nn.Linear(in_feats, out_feats)
+        self.bn = nn.BatchNorm1d(out_feats)
+
+    def forward(self, g, feat_a, feat_b, deg, pm_pd):
+        # term "prev"
+        prev_proj = self.linear_prev(feat_a)
+        # term "deg"
+        deg_proj = self.linear_deg(deg * feat_a)
+
+        # term "radius"
+        # aggregate 2^j-hop features
+        hop2j_list = aggregate_radius(self.radius, g, feat_a)
+        # apply linear transformation
+        hop2j_list = [linear(x) for linear, x in zip(self.linear_radius, hop2j_list)]
+        radius_proj = sum(hop2j_list)
+
+        # term "fuse"
+        fuse = self.linear_fuse(th.mm(pm_pd, feat_b))
+
+        # sum them together
+        result = prev_proj + deg_proj + radius_proj + fuse
+
+        # skip connection and batch norm
+        n = self.out_feats // 2
+        result = th.cat([result[:, :n], F.relu(result[:, n:])], 1)
+        result = self.bn(result)
+
+        return result
+    
+class LGNNLayer(nn.Module):
+    def __init__(self, in_feats, out_feats, radius):
+        super(LGNNLayer, self).__init__()
+        self.g_layer = LGNNCore(in_feats, out_feats, radius)
+        self.lg_layer = LGNNCore(in_feats, out_feats, radius)
+
+    def forward(self, g, lg, x, lg_x, deg_g, deg_lg, pm_pd):
+        next_x = self.g_layer(g, x, lg_x, deg_g, pm_pd)
+        pm_pd_y = th.transpose(pm_pd, 0, 1)
+        next_lg_x = self.lg_layer(lg, lg_x, x, deg_lg, pm_pd_y)
+        return next_x, next_lg_x
+
+class LGNN(nn.Module):
+    def __init__(self, radius):
+        super(LGNN, self).__init__()
+        self.layer1 = LGNNLayer(1, 16, radius)  # input is scalar feature
+        self.layer2 = LGNNLayer(16, 16, radius)  # hidden size is 16
+        self.layer3 = LGNNLayer(16, 16, radius)
+        self.linear = nn.Linear(16, 2)  # predice two classes
+
+    def forward(self, g, lg, pm_pd):
+        # compute the degrees
+        deg_g = g.in_degrees().float().unsqueeze(1)
+        deg_lg = lg.in_degrees().float().unsqueeze(1)
+        # use degree as the input feature
+        x, lg_x = deg_g, deg_lg
+        x, lg_x = self.layer1(g, lg, x, lg_x, deg_g, deg_lg, pm_pd)
+        x, lg_x = self.layer2(g, lg, x, lg_x, deg_g, deg_lg, pm_pd)
+        x, lg_x = self.layer3(g, lg, x, lg_x, deg_g, deg_lg, pm_pd)
+        return self.linear(x)
+
+from torch.utils.data import DataLoader
+training_loader = DataLoader(train_set,
+                             batch_size=1,
+                             collate_fn=train_set.collate_fn,
+                             drop_last=True)
+
+lg = g.line_graph(backtracking=False)
+
+model = LGNN(radius=3)
+# define the optimizer
+optimizer = th.optim.Adam(model.parameters(), lr=1e-1)
+
+# A utility function to convert a scipy.coo_matrix to torch.SparseFloat
+def sparse2th(mat):
+    value = mat.data
+    indices = th.LongTensor([mat.row, mat.col])
+    tensor = th.sparse.FloatTensor(indices, th.from_numpy(value).float(), mat.shape)
+    return tensor
+
+pred2=[]
+# Train for 20 epochs
+for i in range(50):
+    all_loss = []
+    all_acc = []
+    for [g, pmpd, label] in training_loader:
+        # Generate the line graph.
+        lg = g.line_graph(backtracking=False)
+        # Create torch tensors
+        pmpd = sparse2th(pmpd)
+        label = th.from_numpy(label)
+
+        # Forward
+        z = model(g, lg, pmpd)
+
+        #G1, pmpd1, label1
+        
+        lg2=G1.line_graph(backtracking=False)
+        pmpd11=pmpd1
+        pred00=model(G1,lg2,pmpd11)
+        _, pred01 = th.max(pred00, 1)
+        # Calculate loss:
+        # Since there are only two communities, there are only two permutations
+        #  of the community labels.
+        loss_perm1 = F.cross_entropy(z, label)
+        loss_perm2 = F.cross_entropy(z, 1 - label)
+        loss = th.min(loss_perm1, loss_perm2)
+
+        # Calculate accuracy:
+        _, pred = th.max(z, 1)
+        pred2.append(pred01)
+        acc_perm1 = (pred == label).float().mean()
+        acc_perm2 = (pred == 1 - label).float().mean()
+        acc = th.max(acc_perm1, acc_perm2)
+        all_loss.append(loss.item())
+        all_acc.append(acc.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    niters = len(all_loss)
+    print("Epoch %d | loss %.4f | accuracy %.4f" % (i,
+        sum(all_loss) / niters, sum(all_acc) / niters))
+
+
+import time
+def visualize2(labels, g):
+    pos = nx.spring_layout(g, seed=1)
+    plt.axis('off')
+    nx.draw_networkx(g, pos=pos, node_size=50, cmap=plt.get_cmap('coolwarm'),
+                     node_color=labels, edge_color='k',
+                     arrows=False, width=0.5, style='dotted', with_labels=False)
+
+for i in range(0,50):
+    fig = plt.figure(dpi=150)
+    fig.clf()
+    ax = fig.subplots()
+    visualize2(pred2[i].cpu().detach().numpy(),nx_G1)  # draw the prediction of the first epoch
+    plt.savefig('/home/theone/Documents/graph4/foo{}.png'.format(time.time()))
+    #plt.show()
+
+import glob
+from PIL import Image
+
+# filepaths
+fp_in = "/home/theone/Documents/graph4/foo*.png"
+fp_out = "/home/theone/Documents/graph4_GAT_movie.gif"
+
+# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
+img.save(fp=fp_out, format='GIF', append_images=imgs,
+         save_all=True, duration=200, loop=0)
+
+
+
+

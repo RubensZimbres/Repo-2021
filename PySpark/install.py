d4767a6755ee9119f1523d655a5e0798e06c91e6
Rubens Zimbres <rubenszmm@gmail.com>
2021-01-27 11:25:56 -0300

    Update install.py

--- PySpark/install.py
+++ PySpark/install.py
@@ -15,3 +15,16 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("PySpark").getOrCreate()
 spark
+
+data = [['tom', 10], ['nick', 15], ['juli', 14]] 
+
+df = spark.createDataFrame(data,['Name', 'Age']) 
+
+df.show()
+############################
+
+path = "/home/anaconda3/work/Python Files and Datasets AS of 22DEC20/PySpark DataFrame Essentials/Datasets/students.csv"
+df = spark.read.csv(path,header=True)
+df.toPandas()
+
+df.groupBy("gender").agg({'math score':'mean'}).show()

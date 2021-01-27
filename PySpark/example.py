ab95e3d075ab6d234c5fc9012db31401e3a0f466
Rubens Zimbres <rubenszmm@gmail.com>
2021-01-27 14:38:43 -0300

    Update example.py

--- PySpark/example.py
+++ PySpark/example.py
@@ -38,15 +38,22 @@ df.select("gender", "math_score").summary("count", "min", "max").show()
 df.select(['Name','gender']).orderBy('Name').show(5,False) #not truncated
 df.select("Name","math_score").where(df.Name.like("A%")).show(5, False)
 df[df.Name.isin("L. Messi", "Cristiano Ronaldo")].limit(4).toPandas()
-df.filter("Age>40").limit(4).toPandas()
 df.select("Photo",df.Photo.substr(-4,4)).show(5,False) #png
 df.select("Name","math_score").where(df.name.startswith("L")).where(df.Name.like("A%")).show(5, False)
 
-# spark starts with 1 != Python = 0 
+df.filter("Age>40").limit(4).toPandas()
+## spark starts with 1 != Python = 0 
+## filer BEFORE select
 
 # It's not until we change the df in some way, that the ID changes
 # These kinds of commands won't actually be run...
 df = df.withColumn('new_col', df['math score'] * 2)
+
+df2 = df.withColumnRenamed('Rolling year total number of offences','Count')
+
+df.createOrReplaceTempView("tempview")
+spark.sql("SELECT * FROM tempview WHERE Count > 1000").limit(5).toPandas()
+
 
 col_list= df.columns[0:5]
 df3=df.select(col_list)

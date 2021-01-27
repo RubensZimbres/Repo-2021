107c5d08ab19890b4ab26dd8843e9b47d534a194
Rubens Zimbres <rubenszmm@gmail.com>
2021-01-27 11:31:58 -0300

    Update example.py

--- PySpark/example.py
+++ PySpark/example.py
@@ -28,3 +28,14 @@ df = spark.read.csv(path,header=True)
 df.toPandas()
 
 df.groupBy("gender").agg({'math score':'mean'}).show()
+
+# It's not until we change the df in some way, that the ID changes
+# These kinds of commands won't actually be run...
+df = df.withColumn('new_col', df['math score'] * 2)
+
+# Until we executute a command like this
+collect = df.collect()
+
+# Even if we duplicate the dataframe, the ID remains the same
+df2 = df
+df2.rdd.id()

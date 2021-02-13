649a442264705648553af74bfeb70b21993163bd
Rubens Zimbres <rubenszmm@gmail.com>
2021-02-13 12:37:40 -0300

    Update Transformer_Time_Series_YESS.py

--- Transformer/Transformer_Time_Series_YESS.py
+++ Transformer/Transformer_Time_Series_YESS.py
@@ -495,13 +495,15 @@ model = make_model(V, V, N=2)
 model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
         torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
 
-for epoch in range(10):
+#for epoch in range(10):
     #model.train()
-    run_epoch(data_gen(V, 30, 20), model, 
+run_epoch(data_gen(V, 30, 20), model, 
               SimpleLossCompute(model.generator, criterion, model_opt))
     #model.eval()
-    print(run_epoch(data_gen(V, 30, 5), model, 
+print(run_epoch(data_gen(V, 30, 5), model, 
                     SimpleLossCompute(model.generator, criterion, None)))
+
+print('no error until here')
 
 
 
@@ -509,19 +511,20 @@ def greedy_decode(model, src, src_mask, max_len, start_symbol):
     memory = model.encode(src, src_mask)
     ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
     for i in range(max_len-1):
-        out = model.decode(memory, src_mask, 
-                           Variable(ys), 
-                           Variable(subsequent_mask(ys.size(1))
-                                    .type_as(src.data)))
-        prob = model.generator(out[:, -1])
-        _, next_word = torch.max(prob, dim = 1)
-        next_word = next_word.data[0]
-        ys = torch.cat([ys, 
-                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
-    return ys
+#        out = model.decode(memory, src_mask, 
+#                           Variable(ys), 
+#                           Variable(subsequent_mask(ys.size(1))
+#                                    .type_as(src.data)))
+        prob = model.generator(memory[:, -1])
+#        _, next_word = torch.max(prob, dim = 1)
+#        next_word = next_word.data[0]
+#        ys = torch.cat([ys, 
+#                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+    return prob
 
 model.eval()
-src = Variable(torch.LongTensor([[1,2,3,4,5,6,7,8,9,10]]) )
-src_mask = Variable(torch.ones(1, 1, 10) )
 
-print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
+src = Variable(torch.LongTensor(X0[-100:]))
+src_mask = Variable(torch.ones(1, 1, 8 ))
+
+print(greedy_decode(model, src, src_mask, max_len=100, start_symbol=1))

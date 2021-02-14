389d835d6cc80e72ca5bc9f4cc959616d9d15fa9
Rubens Zimbres <rubenszmm@gmail.com>
2021-02-14 20:57:51 -0300

    Update Custom_Layer_PyTorch.py

--- Transformer/Custom_Layer_PyTorch.py
+++ Transformer/Custom_Layer_PyTorch.py
@@ -6,3 +6,14 @@ class Custom(nn.Module):
 
     def forward(self, x):
         return self.w_1(x.view(-1,x.size(0))) * math.sqrt(self.d_model)
+    
+########################################################################
+
+class Custom(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Custom, self).__init__()
+        self.d_model = d_model
+ 
+    def forward(self, x):
+        return torch.cat(4*[x]).reshape(3772,8,self.d_model)  * math.sqrt(self.d_model)
+

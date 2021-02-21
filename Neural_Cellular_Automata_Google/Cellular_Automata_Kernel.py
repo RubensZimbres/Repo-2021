c64465f8e6997557d577e7f6833363a5e6cdf22e
Rubens Zimbres <rubenszmm@gmail.com>
2021-02-20 23:58:35 -0300

    Create Cellular_Automata_Kernel.py

---
+++ Neural_Cellular_Automata_Google/Cellular_Automata_Kernel.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+
+# sets, rows, columns
+## 2D CNN expects a 4D (batch, height, width, channels)
+
+inp=np.arange(75).reshape(1,5,5,3).astype(np.float32)
+
+kernel = np.array([[ [1,1,1],[2,1,2],[1,1,1] ]]).astype(np.float32).reshape(1,3,3,1)
+
+print('input shape ->',inp.shape)
+print('kernel shape ->',kernel.shape)
+
+result = tf.nn.conv2d(inp, kernel, strides=(1,1,1,1), padding='VALID')
+
+[[      [[ 44.],
+         [ 77.],
+         [110.]],
+
+        [[209.],
+         [242.],
+         [275.]],
+
+        [[374.],
+         [407.],
+         [440.]],
+
+        [[539.],
+         [572.],
+         [605.]],
+
+        [[704.],
+         [737.],
+         [770.]]   ]]

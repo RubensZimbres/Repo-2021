67370b02450e7d055b84ea01100b3880a7b22b2c
Rubens Zimbres <rubenszmm@gmail.com>
2021-02-24 00:23:42 -0300

    Create CA_Kernel_function.py

---
+++ Cellular_Automata/CA_Kernel_function.py
@@ -0,0 +1,36 @@
+import numpy as np
+import itertools
+regra=2159062512564987644819455219116893945895958528152021228705752563807958532187120148734120
+
+kernel=np.random.randint(2, size=(3,3))
+
+
+def cellular_automaton():
+    global kernel
+
+    lista=[0,1,2,3,4]
+    kernel=np.pad(kernel, (1, 1), 'constant', constant_values=(0))
+    q12=np.array([p for p in itertools.product(lista, repeat=3)])[::-1]
+
+    uau12 = np.base_repr(int(regra),base=5)
+
+    ru12=np.array(range(0,len(uau12)))
+
+    tod12=[]
+    for i in range(0,len(uau12)):
+        tod12.append([0,int(uau12[i]),0])
+
+    final=[]
+    for i in range(0,len(q12)):
+        final.append(np.array([q12[i],np.array(tod12).astype(np.int8)[i]]))
+
+    
+    def ca(row):
+        out=[]
+        for xx in range(0,3):
+            out.append(tod12[next((i for i, val in enumerate(q12) if np.all(val == kernel[row][xx:xx+3])), -1)][1])
+        return out
+    kernel=np.array([item for item in map(ca,range(1,kernel.shape[0]-1))])
+    return kernel
+
+cellular_automaton()

a2cf9ee7e71e2666ffc8a6108f3fdf4eaaf3dfec
Rubens Zimbres <rubenszmm@gmail.com>
2021-02-25 09:55:11 -0300

    Update PyTorch_CNN_Custom_kernel_CA.py

--- Cellular_Automata/PyTorch_CNN_Custom_kernel_CA.py
+++ Cellular_Automata/PyTorch_CNN_Custom_kernel_CA.py
@@ -50,24 +50,27 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=10,
-                                          shuffle=True, num_workers=2)
+train_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=128, shuffle=True)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=10,
-                                         shuffle=False, num_workers=2)
+test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=1000, shuffle=True)
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+examples = enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
 
-input_size = 784
-hidden_sizes = [128, 64]
-output_size = 10
 
 import torch.nn.functional as F
 import torch.nn as nn
@@ -75,88 +78,82 @@ import torch.nn as nn
 class Net(nn.Module):
     def __init__(self,kernel):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 3,bias=False)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 3,bias=False)
-        self.fc1 = nn.Linear(800, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 100)
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5,bias=False)
+        self.conv2 = nn.Conv2d(1, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
         self.conv1.weight = nn.Parameter(kernel,requires_grad=True)
 
-
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x))).view(5,6,-1,16)
-        #print(x.shape)
-        x = self.pool(F.relu(self.conv2(x).view(10,1,16,21)))
-        x = x.view(-1, 800)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        
-        #print('output',x.shape)
-        return F.log_softmax(x,dim=1).view(-1,10)
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
 
 import torch.optim as optim
 
 PATH = './cifar_net.pth'
 
-c=torch.from_numpy(cellular_automaton()[1:4,1:4].astype(np.float16).reshape(1,3,3,1)).type(torch.FloatTensor)
+c=torch.from_numpy(cellular_automaton().astype(np.float16).reshape(1,1,5,5)).type(torch.FloatTensor)
 #print(c)
 net = Net(c)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 #net.load_state_dict(torch.load(PATH))
-loss = nn.CrossEntropyLoss()
-input = torch.randn(3, 5, requires_grad=True)
-target = torch.empty(3, dtype=torch.long).random_(5)
-output = loss(input, target)
-output.backward()
+
+n_epochs = 3
+batch_size_train = 128
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
+
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+def train(epoch):
+  net.train()
+  for batch_idx, (data, target) in enumerate(train_loader):
+    optimizer.zero_grad()
+    output = net(data)
+    loss = F.nll_loss(output, target)
+    loss.backward()
+    optimizer.step()
+    if batch_idx % log_interval == 0:
+      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        epoch, batch_idx * len(data), len(train_loader.dataset),
+        100. * batch_idx / len(train_loader), loss.item()))
+      train_losses.append(loss.item())
+      train_counter.append(
+        (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+      torch.save(net.state_dict(), './results/model.pth')
+      torch.save(optimizer.state_dict(), './results/optimizer.pth')
+
+
+def test():
+  net.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in test_loader:
+      output = net(data)
+      test_loss += F.nll_loss(output, target, size_average=False).item()
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+  test_loss /= len(test_loader.dataset)
+  test_losses.append(test_loss)
+  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
 
 
 
-
-
-
-for epoch in range(2):  # loop over the dataset multiple times
-
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        #print(inputs.shape)
-        #print(labels.shape)
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        ##print(outputs)
-        #print(labels)
-        loss = criterion(outputs, labels)
-        #print(loss)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-    torch.save(net.state_dict(), PATH)
-
-
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
+for epoch in range(1, n_epochs + 1):
+  train(epoch)
+  test()

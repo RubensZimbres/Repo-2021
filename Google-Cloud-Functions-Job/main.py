274b4d0ffcfa1dc4d97d54eb7df4cb0c2285b168
Rubens Zimbres <rubenszmm@gmail.com>
2021-03-07 10:08:41 -0300

    Update main.py

--- Google-Cloud-Functions-Job/main.py
+++ Google-Cloud-Functions-Job/main.py
@@ -2,8 +2,8 @@ import datetime
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-id = 'machinelearning-XXXX'
-bucket_name = "bucket-XXX"
+id = 'machinelearning-XXX'
+bucket_name = "keras-XXX-folder"
 project_id = 'projects/{}'.format(id)
 job_name = "training_" + datetime.datetime.now().strftime("%y%m%d_%H%M%S")
 
@@ -11,10 +11,10 @@ def main(event, context):
 
      training_inputs = {
      'scaleTier': 'BASIC',
-     'packageUris': [f"gs://{bucket_name}/package/trainer-0.1.tar.gz"],
+     'packageUris': ["gs://{}/trainer".format(bucket_name)],
      'pythonModule': 'trainer.task',
-     'region': 'asia-northeast1',
-     'jobDir': f"gs://{bucket_name}",
+     'region': 'us-central1',
+     'jobDir': "gs://{}".format(bucket_name),
      'runtimeVersion': '2.2',
      'pythonVersion': '3.7',
           }

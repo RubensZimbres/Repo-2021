d701dd8d979faf7bdcc4983b7524bbdc0aaf8e7f
Rubens Zimbres <rubenszmm@gmail.com>
2021-03-12 18:00:06 -0300

    Create custom-training.py

---
+++ Hugging_Face/custom-training.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir/label_dir).iterdir():
+            texts.append(text_file.read_text())
+            labels.append(0 if label_dir is "neg" else 1)
+
+    return texts, labels
+
+train_texts, train_labels = read_imdb_split('aclImdb/train')
+test_texts, test_labels = read_imdb_split('aclImdb/test')
+
+from sklearn.model_selection import train_test_split
+train_texts, val_texts, train_labels, val_labels = train_test_split(train_texts, train_labels, test_size=.2)
+
+from transformers import DistilBertTokenizerFast
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+import torch
+
+class IMDbDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset = IMDbDataset(train_encodings, train_labels)
+val_dataset = IMDbDataset(val_encodings, val_labels)
+test_dataset = IMDbDataset(test_encodings, test_labels)
+
+from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+
+training_args = TrainingArguments(
+    output_dir='./results',          
+    num_train_epochs=3,              
+    per_device_train_batch_size=16,  
+    per_device_eval_batch_size=64,   
+    warmup_steps=500,                
+    weight_decay=0.01,               
+    logging_dir='./logs',            
+    logging_steps=10,
+)
+
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+
+trainer = Trainer(
+    model=model,                         
+    args=training_args,                  
+    train_dataset=train_dataset,         
+    eval_dataset=val_dataset             
+)
+
+trainer.train()
+
+from torch.utils.data import DataLoader
+from transformers import DistilBertForSequenceClassification, AdamW
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+model.to(device)
+model.train()
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+optim = AdamW(model.parameters(), lr=5e-5)
+
+for epoch in range(3):
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.eval()

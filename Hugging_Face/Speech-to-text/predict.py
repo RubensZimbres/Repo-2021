26fe6c587699729af83a5a920c45bd7cac5ddb38
Rubens Zimbres <rubenszmm@gmail.com>
2021-03-30 18:13:52 -0300

    Create predict.py

---
+++ Hugging_Face/Speech-to-text/predict.py
@@ -0,0 +1,35 @@
+# https://huggingface.co/jonatasgrosman/wav2vec2-large-xlsr-53-portuguese
+# https://huggingface.co/Rubens/Wav2Vec2-Large-XLSR-53-a-Portuguese
+
+import torch
+import librosa
+from datasets import load_dataset
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+
+LANG_ID = "pt"
+MODEL_ID = "/home/anaconda3/work/wav2vec2-large-xlsr-53-portuguese"
+#MODEL_ID = "/home/anaconda3/work/Wav2Vec2-Large-XLSR-53-a-Portuguese"
+
+test_dataset = load_dataset("common_voice", LANG_ID, split="test[:2%]")
+
+processor = Wav2Vec2Processor.from_pretrained(MODEL_ID)
+model = Wav2Vec2ForCTC.from_pretrained(MODEL_ID)
+
+# Preprocessing the datasets
+# We need to read the audio files as arrays
+def speech_file_to_array_fn(batch):
+    speech_array, sampling_rate = librosa.load(batch["path"], sr=16_000)
+    batch["speech"] = speech_array
+    batch["sentence"] = batch["sentence"].upper()
+    return batch
+
+test_dataset = test_dataset.map(speech_file_to_array_fn)
+inputs = processor(test_dataset[:2]["speech"], sampling_rate=16_000, return_tensors="pt", padding=True)
+
+with torch.no_grad():
+    logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
+
+predicted_ids = torch.argmax(logits, dim=-1)
+
+print("Prediction:", processor.batch_decode(predicted_ids))
+print("Reference:", test_dataset[:2]["sentence"])

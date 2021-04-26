109c85874495f45d09145cb2c8d1fdb15fe2c956
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-26 20:53:44 -0300

    Update to_binary.cpp

--- C-Language/Cellular_Automaton/to_binary.cpp
+++ C-Language/Cellular_Automaton/to_binary.cpp
@@ -7,17 +7,17 @@ int main()
     ///printf("Enter the number to convert: ");    
     ///scanf("%d",&dno);    
     dno=i;
-    g=0;
-    while(g<3)
+    g=2;
+    while(g>=0)
     {   
         remainder = dno % 2;
         dno = dno / 2;
         bno = remainder;
         a[i][g]=bno;
-        g=g+1;
+        g=g-1;
     }
 
-    printf("\nBinary of Given Number is:\n%d%d%d\n",a[i][2],a[i][1],a[i][0]);    
+    printf("\nBinary of Given Number is:\n%d%d%d\n",a[i][0],a[i][1],a[i][2]);    
     }
     return 0;  
 }  

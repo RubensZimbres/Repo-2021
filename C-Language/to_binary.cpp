ed492cb3bb8f83e58d70a30f23ba89b0962e945c
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-26 20:46:53 -0300

    Update and rename binary_test.cpp to to_binary.cpp

---
+++ C-Language/to_binary.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>    
+#include<stdlib.h>  
+int main()
+{  
+    int a[8][3],i,dno,bno=0,remainder,f=1,g=0;
+    for (i=0;i<8;i=i+1){
+    ///printf("Enter the number to convert: ");    
+    ///scanf("%d",&dno);    
+    dno=i;
+    g=0;
+    while(g<3)
+    {   
+        remainder = dno % 2;
+        dno = dno / 2;
+        bno = remainder;
+        a[i][g]=bno;
+        g=g+1;
+    }
+
+    printf("\nBinary of Given Number is:\n%d%d%d\n",a[i][2],a[i][1],a[i][0]);    
+    }
+    return 0;  
+}  

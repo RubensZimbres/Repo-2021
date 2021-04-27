4b22d60a0f6207e76ea601f88cb02f3383367b98
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-27 09:47:41 -0300

    Update function_c.cpp

--- C-Language/function_c.cpp
+++ C-Language/function_c.cpp
@@ -2,17 +2,27 @@
 
 int myGlobal=0;
 
-int multiply(int x, int y)
-{   int result;
-    int myLocal;
-    result = x*y;
-    return result;
-}
+int multiply(int x, int y);
+int AbsValue(int a);
 
 int main(void)
 {   int result;
-    result=multiply(2,3);
+    result=multiply(-3,-3);
     printf("\n%i\n",result);
     
     return 0;
+}
+
+int multiply(int x, int y)
+{   int result;
+    int myLocal;
+    result = AbsValue(x)*AbsValue(y);
+    return result;
+}
+
+int AbsValue(int a)
+{
+    if (a < 0)
+    a=-a;
+    return a;
 }

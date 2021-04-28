31f55b922d9ca0665f2497c7ea0e2e1281a76af7
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-28 16:11:50 -0300

    Update to_binary.cpp

--- C-Language/Cellular_Automaton/to_binary.cpp
+++ C-Language/Cellular_Automaton/to_binary.cpp
@@ -2,22 +2,24 @@
 #include<stdlib.h>  
 int main()
 {  
-    int a[8][3],i,dno,bno=0,remainder,f=1,g=0;
-    for (i=0;i<8;i=i+1){
-    ///printf("Enter the number to convert: ");    
-    ///scanf("%d",&dno);    
-    dno=i;
-    g=2;
-    while(g>=0)
+    int a[3];
+    int input1;
+    int *dno=&input1;
+    int bno=0;
+    int remainder;
+    int g=0;
+    printf("Enter the number to convert: ");    
+    scanf("%d",&input1);    
+
+    while(g<3)
     {   
-        remainder = dno % 2;
-        dno = dno / 2;
+        remainder = *dno % 2;
+        *dno = *dno / 2;
         bno = remainder;
-        a[i][g]=bno;
-        g=g-1;
+        a[g]=bno;
+        g=g+1;
     }
 
-    printf("\nBinary of Given Number is:\n%d%d%d\n",a[i][0],a[i][1],a[i][2]);    
-    }
+    printf("\nBinary of Given Number is:\n%d%d%d\n",a[2],a[1],a[0]);    
     return 0;  
 }  

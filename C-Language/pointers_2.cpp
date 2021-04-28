ce81fca5f69301c6cf8c3590c023cc2ac9c97114
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-27 22:09:18 -0300

    Create pointers_2.cpp

---
+++ C-Language/pointers_2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void square(int  * x);
+
+int main(){
+    ///int num=9;
+    int *num =(int*)malloc(sizeof(int));
+    *num=4;
+    square(num);
+    printf("Square on given is:\n %d",*num);
+    return 0;
+}
+
+void square(int * x){
+
+    *x=(*x) * (*x); /// resultado usa mesmo espaço na memória
+
+}

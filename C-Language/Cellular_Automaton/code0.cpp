7da89a411ffdedfac6de526b05cb3fd7ab05ff9f
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-29 11:45:11 -0300

    Update code0.cpp

--- C-Language/Cellular_Automaton/code0.cpp
+++ C-Language/Cellular_Automaton/code0.cpp
@@ -8,7 +8,9 @@ int main()
     int posicao[8];
     int states=2;
     int multi[8];
-    int regra;
+    int regra=0;
+    int *pregra=&regra;
+    while (regra<=255){
     int outputs[8];
     int saida[8];
     printf("Which CA rule ?");
@@ -17,12 +19,13 @@ int main()
     {
     posicao[i] = i;
     multi[i] = pow(states,i);
-    outputs[i]=regra/multi[i];
+    outputs[i]=*pregra/multi[i];
     saida[i]=outputs[i]%2;
     }
 
 
     printf("\n%d%d%d%d%d%d%d%d\n",saida[0],saida[1],saida[2],saida[3],saida[4],saida[5],saida[6],saida[7]);
+    }
     return 0;
     
 }

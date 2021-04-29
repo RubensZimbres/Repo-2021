31ebe20f8c8efe4f11567c4c1dabee7b52465df4
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-29 10:54:45 -0300

    Update to_binary.cpp

--- C-Language/Cellular_Automaton/to_binary.cpp
+++ C-Language/Cellular_Automaton/to_binary.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>    
-#include<stdlib.h>  
+#include<stdlib.h> 
+
 int main()
 {  
     int a[3];
     int input1;
     int *dno=&input1;
-    int bno=0;
     int remainder;
     int g=0;
     printf("Enter the number to convert: ");    
@@ -15,8 +15,7 @@ int main()
     {   
         remainder = *dno % 2;
         *dno = *dno / 2;
-        bno = remainder;
-        a[g]=bno;
+        a[g] = remainder;
         g=g+1;
     }
 

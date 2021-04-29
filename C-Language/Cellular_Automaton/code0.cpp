a385f75a3edea2f0bd3e66ce6df394855e399045
Rubens Zimbres <rubenszmm@gmail.com>
2021-04-29 11:07:20 -0300

    Update code0.cpp

--- C-Language/Cellular_Automaton/code0.cpp
+++ C-Language/Cellular_Automaton/code0.cpp
@@ -3,31 +3,20 @@
 
 
 int main()
-{   fflush( stdout );
+{   
     int i;
-    int in[3]={0,1,1};
-    int arr[3];
     int posicao[8];
     int states=2;
     int multi[8];
-    int regra=232;
-    int inputs[8];
+    int regra;
     int outputs[8];
     int saida[8];
-    int binary[8];
-    int dno;
+    printf("Which CA rule ?");
+    scanf("%d",&regra);
     for (i = 0; i <8; i++)
     {
     posicao[i] = i;
-    }
-
-    for (i = 0; i <8; i++)
-    {
     multi[i] = pow(states,i);
-    }
-
-    for (i = 0; i <8; i++)
-    {
     outputs[i]=regra/multi[i];
     saida[i]=outputs[i]%2;
     }

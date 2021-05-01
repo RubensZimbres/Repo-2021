1055a31a8c920af756d6600ef9f8ad0ad4c4df9d
Rubens Zimbres <rubenszmm@gmail.com>
2021-05-01 14:31:22 -0300

    Update cellular_automaton_yess.ino

--- C-Language/Cellular_Automaton/cellular_automaton_yess.ino
+++ C-Language/Cellular_Automaton/cellular_automaton_yess.ino
@@ -7,8 +7,9 @@ void setup()
   Serial.begin(9600);
 }
 
-void loop()
-{   int* ca_final = (int *) malloc(sizeof(int));
+int ca_final[6]={1,0,0,1,0,1};
+
+void loop(){
 
     int array[8][3]={{0, 0, 0},
        {0, 0, 1},
@@ -18,15 +19,9 @@ void loop()
        {1, 0, 1},
        {1, 1, 0},
        {1, 1, 1}};
+    
     int m=0;
-    if (m==0){
-      ca_final[0]=1;
-      ca_final[3]=1;
-      ca_final[5]=1;
-    }
 
-
-      delay(3000);
     m=m+1;
     int k=0;
     int n = 5;
@@ -46,7 +41,6 @@ void loop()
         split_array[k][2]=ca_final[(int)(k+1)];
     }
     int i=0;
-    int ii=0;
     int posicao[8];
     int states=2;
     int multi[8];
@@ -67,7 +61,7 @@ void loop()
         outputs[i]=regra/multi[i];
         saida[i]=outputs[i]%2;
         ///printf("\n%d\n",saida[i]);
-    for (ii = 0; ii <6; ii++){
+    for (int ii = 0; ii <6; ii++){
         for (iter2=0;iter2<6;++iter2){
             if (split_array[ii][0]==array[iter2][0] && split_array[ii][1]==array[iter2][1] && split_array[ii][2]==array[iter2][2]){
                 ca_final[ii]=saida[iter2];
@@ -77,13 +71,12 @@ void loop()
     ///printf("\n%d%d%d%d%d%d%d%d\n",saida[0],saida[1],saida[2],saida[3],saida[4],saida[5],saida[6],saida[7]);
     }
     }
-    ///printf("\n%d%d%d%d%d%d\n", ca_final[0], ca_final[1], ca_final[2], ca_final[3], ca_final[4], ca_final[5]);
-    for (int p=0;p<6;p++) {
+    printf("\n%d%d%d%d%d%d\n", ca_final[0], ca_final[1], ca_final[2], ca_final[3], ca_final[4], ca_final[5]);
+for (int p=0;p<6;p++) {
       Serial.print(ca_final[p],DEC);
-      }
-    Serial.println();
 
 
-    ///return 0;
+    }
     
+    Serial.println();
 }

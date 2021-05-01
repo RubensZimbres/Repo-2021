d7851b271b0808d9ae74c8dde875d90367cc312b
Rubens Zimbres <rubenszmm@gmail.com>
2021-05-01 15:47:46 -0300

    Update cellular_automaton_yess.ino

--- C-Language/Cellular_Automaton/cellular_automaton_yess.ino
+++ C-Language/Cellular_Automaton/cellular_automaton_yess.ino
@@ -2,14 +2,23 @@
 #include <math.h>
 #include<stdlib.h> 
 
+int ca_final[6]={1,0,0,1,0,1};
+
 void setup() 
 {
   Serial.begin(9600);
 }
 
-int ca_final[6]={1,0,0,1,0,1};
 
 void loop(){
+    delay(3000);
+    for (int p=0;p<6;p++) {
+      Serial.print(ca_final[p],DEC);
+
+
+    }
+    
+    Serial.println();
 
     int array[8][3]={{0, 0, 0},
        {0, 0, 1},
@@ -26,9 +35,7 @@ void loop(){
     int k=0;
     int n = 5;
     int split_array[6][3]; 
-    ///={{1,1,0},{1,0,0},{0,0,1},{0,1,0},{1,0,1},{0,1,1}};
-    /// comparar split_array com array e retornar o indice do resultado e busca no resultado
-    /// size dividido por 4 bytes
+
     split_array[0][0]=ca_final[n];
     split_array[0][1]=ca_final[0];
     split_array[0][2]=ca_final[1];
@@ -47,36 +54,19 @@ void loop(){
     int regra=30;
     
     int outputs[8];
-    int saida[8];
+    int saida[8]={0,1,1,1,1,0,0,0};
 
     
     int iter=0;
     int iter2=0;
 
-    ///printf("Which CA rule ?");
-    ///scanf("%d",&regra);
-    for (i = 0; i <8; i++){
-        posicao[i] = i;
-        multi[i] = pow(states,i);
-        outputs[i]=regra/multi[i];
-        saida[i]=outputs[i]%2;
-        ///printf("\n%d\n",saida[i]);
     for (int ii = 0; ii <6; ii++){
         for (iter2=0;iter2<6;++iter2){
             if (split_array[ii][0]==array[iter2][0] && split_array[ii][1]==array[iter2][1] && split_array[ii][2]==array[iter2][2]){
                 ca_final[ii]=saida[iter2];
     
     }
-    }
-    ///printf("\n%d%d%d%d%d%d%d%d\n",saida[0],saida[1],saida[2],saida[3],saida[4],saida[5],saida[6],saida[7]);
-    }
-    }
-    printf("\n%d%d%d%d%d%d\n", ca_final[0], ca_final[1], ca_final[2], ca_final[3], ca_final[4], ca_final[5]);
-for (int p=0;p<6;p++) {
-      Serial.print(ca_final[p],DEC);
-
-
-    }
     
-    Serial.println();
+    }
+    }
 }

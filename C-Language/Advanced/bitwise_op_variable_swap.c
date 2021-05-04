a6222587d23fa32647e35d90394de3e6a81f9ad4
Rubens Zimbres <rubenszmm@gmail.com>
2021-05-04 12:03:26 -0300

    Update bitwise_op_variable_swap.c

--- C-Language/Advanced/bitwise_op_variable_swap.c
+++ C-Language/Advanced/bitwise_op_variable_swap.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    short int w1=147;
-    /// 10010011
-    short int w2=61;
-    /// 00111101
-    short int temp=0;
-    temp=w1;
-    w1=w2;
-    w2=temp;
+    short int w1=4;
+    /// 00000100
+    short int w2=2;
+    /// 00000010
+    printf("\nbefore w1 =%d and w2 = %d\n",w1,w2);
+    
+    //short int temp=0;
+    //temp=w1;
+    //w1=w2;
+    //w2=temp;
 
     // goes to
 
     w1^=w2;
     w2^=w1;
     w1^=w2;
-
+    printf("\nafter w1 =%d and w2 = %d\n",w1,w2);
     return 0;
 }

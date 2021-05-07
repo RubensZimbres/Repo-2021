d635dbd2a56ddb941272887397c942a9885c32ff
Rubens Zimbres <rubenszmm@gmail.com>
2021-05-07 15:22:35 -0300

    Create start.py

---
+++ PyTorch-Geometric/start.py
@@ -0,0 +1,67 @@
+import torch
+from torch_geometric.data import Data
+
+
+# Transfer data object to GPU.
+
+from torch_geometric.datasets import TUDataset, Planetoid
+from torch_geometric.data import DataLoader
+
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
+for batch in loader:
+    print(batch)
+    print(batch.num_graphs) 
+
+#data.num_classes
+
+#data.num_edges
+
+#data.num_node_features
+
+#data.contains_isolated_nodes()
+
+#data.contains_self_loops()
+
+#data.is_directed()
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim=1)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net().to(device)
+data = dataset[0].to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+model.train()
+
+for epoch in range(200):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out, data.y)
+    loss.backward()
+    optimizer.step()
+
+
+model.eval()
+_, pred = model(data).max(dim=1)
+correct = int(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+acc = correct / int(data.test_mask.sum())
+print('Accuracy: {:.4f}'.format(acc))

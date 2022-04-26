6d57eb7a58f75b30fa869153e7c620e8a056d5e2
Rubens Zimbres <rubenszmm@gmail.com>
2022-04-26 12:06:06 -0300

    Update PyTorch_CNN_Custom_kernel_CA_Pred_TEST_2Kernel_Residual**.py

--- Cellular_Automata/PyTorch_CNN_Custom_kernel_CA_Pred_TEST_2Kernel_Residual**.py
+++ Cellular_Automata/PyTorch_CNN_Custom_kernel_CA_Pred_TEST_2Kernel_Residual**.py
@@ -50,28 +50,25 @@ print(cellular_automaton()[1:4,1:4])
 
 device = torch.device("cuda")
 
+batch_size1=500
 
 train_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('./', train=True, download=True,
+  torchvision.datasets.MNIST('/home/theone/other_models/mnist', train=True, download=True,
                              transform=torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
                                torchvision.transforms.Normalize(
                                  (0.0,), (1.,))
                              ])),
-  batch_size=1000, shuffle=True)
+  batch_size=batch_size1, shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('./', train=False, download=True,
+  torchvision.datasets.MNIST('/home/theone/other_models/mnist', train=False, download=True,
                              transform=torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
                                torchvision.transforms.Normalize(
                                  (0.0,), (1,))
                              ])),
-  batch_size=1000, shuffle=True)
-
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-
+  batch_size=batch_size1, shuffle=True)
 
 
 class Net(nn.Module):
@@ -79,28 +76,37 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(32, 64, 3, 1,bias=False)
         self.conv2 = nn.Conv2d(32, 64, 3, 1,bias=False)
-        self.dropout1 = nn.Dropout(0.1)
-        self.dropout2 = nn.Dropout(0.2)
-        self.fc1 = nn.Linear(400, 28*28)
+        self.conv3 = nn.Conv2d(1, 64, 10, 1,bias=False)
+        self.dropout1 = nn.Dropout(0.2)
+        self.dropout2 = nn.Dropout(0.4)
+        self.fc1 = nn.Linear(64, 28*28)
         self.fc2 = nn.Linear(28*28, 1024)
         self.fc3 = nn.Linear(1024, 1024)
         self.fc4 = nn.Linear(1024, 10)
+        torch.nn.init.xavier_uniform(self.fc1.weight)
+        torch.nn.init.xavier_uniform(self.fc2.weight)
+        torch.nn.init.xavier_uniform(self.fc3.weight)
+        torch.nn.init.xavier_uniform(self.fc4.weight)
+        self.batch_norm = nn.BatchNorm1d(64)
         self.conv1.weight = nn.Parameter(kernel,requires_grad=False)
         self.conv2.weight = nn.Parameter(kernel,requires_grad=False)
 
 
     def forward(self, x):
-        res=x.view(1000, 784)
+        res = x.view(batch_size1, 784)
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
+        x = nn.MaxPool2d(2, 2)(x)
+        x = self.conv3(x)
+        x = F.relu(x)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
+        #x = self.batch_norm(x)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout2(x)
-        #x = self.fc2(torch.cat((torch.flatten(x),torch.flatten(res)),0).view(-1,1568))
         x = self.fc2(torch.mean(torch.stack((x,res)),0))
         x = F.relu(x)
         x = self.fc3(x)
@@ -112,8 +118,8 @@ class Net(nn.Module):
 import torch.optim as optim
 
 #Adadelta 1000 epochs
-n_epochs = 100
-learning_rate = 0.009
+n_epochs = 380
+learning_rate = 0.01
 log_interval = 10
 
 train_losses = []
@@ -131,7 +137,7 @@ optimizer = optim.SGD(net.parameters(), lr=learning_rate)
 
 def train(epoch):
   net.train()
-  for batch_idx, (data, target) in enumerate(train_loader):    
+  for batch_idx, (data, target) in enumerate(train_loader):
     data, target = data.to(device), target.to(device)
     optimizer.zero_grad()
     output = net(data)
@@ -153,10 +159,14 @@ def test():
     with torch.no_grad():        
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output = net(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item()
-            pred = output.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).sum()
+            #print(data.shape)
+            try:
+              output = net(data)
+              test_loss += F.nll_loss(output, target, size_average=False).item()
+              pred = output.max(1, keepdim=True)[1]
+              correct += pred.eq(target.data.view_as(pred)).sum()
+            except:
+              pass
         test_loss /= len(test_loader.dataset)
         test_losses.append(test_loss)
         print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.6f}%)\n'.format(

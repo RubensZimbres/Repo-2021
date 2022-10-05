b67f6fe61a8c8066471c59646198ac9d208e2699
Rubens Zimbres <rubenszmm@gmail.com>
2022-10-05 16:53:19 -0300

    Update Iterate_all_directories.py

--- Iterate_all_directories.py
+++ Iterate_all_directories.py
@@ -16,3 +16,24 @@ for arquivo in locais:
     for line in open(arquivo):
         jsons.append(json.loads(line))
 pd.DataFrame.from_records(jsons).head()
+
+
+--------------------------------------------
+
+
+import pandas as pd
+import os
+import numpy as np
+rootdir='/home/user/Downloads/other_models/prediction'
+
+df=pd.read_csv(rootdir+'/'+sorted(os.listdir(rootdir))[1],sep=',',header=0)
+
+df0=[]
+
+for files in sorted(os.listdir(rootdir)):
+    df0.append(pd.read_csv(rootdir+'/'+files,sep=',',header=0))
+    print(files)
+
+df0[0]
+
+result_prev=pd.concat(df0,axis=0)
